5a66b53743ad49e2f7db4ec5971ce243f260c9f9
William Kim <william.kim@perfectmind.com>
2022-02-19 01:33:15 -0800

    js lec1

---
+++ web/script.js
@@ -0,0 +1,39 @@
+// let country ="Kr";
+// let continent ="Van";
+// let populaton =32;
+
+// console.log(country, continent, populaton);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun)
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "Jonas");
+
+// javascriptIsFun = "Yes!";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// birthYear = 2002;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "kim";
+console.log(lastName);
